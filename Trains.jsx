07cd018a270e69e3ad57de075436d941b5d93e4f
Harmanpreet Kaur <singhpardeepnirman96@gmail.com>
2026-09-13 19:19:36 +0530

    Create Trains.jsx

---
+++ Trains.jsx
@@ -0,0 +1,39 @@
+const trains = [
+  {
+    id: 1,
+    name: "Shatabdi Express",
+    number: "12001",
+    from: "Ludhiana",
+    to: "New Delhi",
+    departure: "06:00 AM",
+    arrival: "10:30 AM",
+    fare: 850,
+    seats: 42
+  },
+
+  {
+    id: 2,
+    name: "Rajdhani Express",
+    number: "12424",
+    from: "Ludhiana",
+    to: "New Delhi",
+    departure: "08:00 AM",
+    arrival: "12:30 PM",
+    fare: 1200,
+    seats: 28
+  },
+
+  {
+    id: 3,
+    name: "Intercity Express",
+    number: "12460",
+    from: "Ludhiana",
+    to: "New Delhi",
+    departure: "02:15 PM",
+    arrival: "07:00 PM",
+    fare: 650,
+    seats: 55
+  }
+];
+
+export default trains;
